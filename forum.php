5ad3d40d0f551a6ffbc603ebeef96e062e6e3f86
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2021-01-07 18:36:00 +0800

    Question list and forum pages done

--- forum.php
+++ forum.php
@@ -31,24 +31,120 @@
 </head>
 
 <body>
-    <!-- Header -->
     <header class="fixed-top">
         <nav class="navbar navbar-light header scrolling-navbar">
             <div class="container-fluid">
-                <a href="https://www.nightcatdigitalsolutions.com" class="navbar-brand">
-                    <img class="img-fluid rounded" src="./assets/titleImage.jpeg" alt="Title Image">
-                </a>
+                <div class="row" style="width:100%;">
+                    <div class="col-4">
+
+                    </div>
+                    <div class="col-4">
+                        <a href="https://www.nightcatdigitalsolutions.com" class="navbar-brand mx-auto">
+                            <img class="img-fluid rounded" src="./assets/titleImage.jpeg" alt="Title Image">
+                        </a>
+                    </div>
+                    <div class="col-4 my-auto">
+                        <div class="row">
+                            <div class="col-6 text-right">
+                                <h5></h5>
+                            </div>
+                            <div class="col-6 text-left">
+                                <button class="btn py-3 text-white loginBtn">Login/Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </nav>
+        <div class="bg-white py-4">
+        </div>
     </header>
 
     <!-- Main Section -->
     <main class="main-section">
+        <div class="container">
+            <section class="container stickyContent bg-white position-fixed pt-3">
+                <h1 class="py-2">Question 1</h1>
+                <div class="row py-3 pb-4">
+                    <div class="col-6 text-left my-auto">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb bg-white">
+                                <li class="breadcrumb-item"><a href="./list.php">Question</a></li>
+                                <li class="breadcrumb-item active">Question 1</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </section>
 
+            <section class="answerList">
+                <div class="card my-3">
+                    <div class="card-body p-4 px-5">
+                        <div class="row">
+                            <div class="col-3">
+                                <h2 class="card-title">Question 1</h2>
+                            </div>
+                            <div class="col-7 py-auto">
+                                <h5>from tester 1</h5>
+                            </div>
+                            <div class="col-2 py-auto text-right">
+                                <p>2020-09-28 9:30am</p>
+                            </div>
+                        </div>
+                        <div class="row py-3">
+                            <p class="card-text text-justify" style="font-size:20px;">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt eius placeat consequuntur odio sed, aperiam deserunt commodi esse laudantium libero doloribus ullam facere,laudantium libero doloribus ullam facerelaudantium libero doloribus ullam facere ...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card my-3">
+                    <div class="card-body p-4 px-5">
+                        <div class="row">
+                            <div class="col-10">
+                                <h4 class="card-title">tester 2</h4>
+                            </div>
+                            <div class="col-2 py-auto text-right">
+                                <p>2020-09-28 12:30pm</p>
+                            </div>
+                        </div>
+                        <div class="row py-3">
+                            <p class="card-text text-justify" style="font-size:16px;">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt eius placeat consequuntur odio sed, aperiam deserunt commodi esse laudantium libero doloribus ullam facere,laudantium libero doloribus ullam facerelaudantium libero doloribus ullam facere ...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="py-4" id="pagination">
+
+            </section>
+
+            <section class="postAnswer container">
+                <div class="md-form mb-4 pink-textarea active-pink-textarea">
+                    <div class="row p-4" style="background-color:#0774dd; border-radius:20px 20px 5px 5px;">
+                        <div class="col-6 text-left my-auto">
+                            <h4 class="text-white">Share your answer</h4>
+                        </div>
+                        <div class="col-6 text-right">
+                            <button class="btn btn-primary postAnswerBtn px-5 py-2">Post</button>
+                        </div>
+                    </div>
+                    <div class="row p-4" style="background-color:#e1e1e1;border-radius:5px 5px 20px 20px;">
+                        <textarea class="md-textarea form-control" placeholder="Place your answer here" rows="5"></textarea>
+                    </div>
+                </div>
+            </section>
+
+
+
+
+        </div>
     </main>
 
     <!-- Footer -->
-    <footer class="page-footer font-small mdb-color blue" style="width: auto;">
+    <footer class=" page-footer font-small mdb-color blue" style="width: auto;">
         <div class="col-md-12">
 
             <!-- Social Link on Bottom -->
@@ -77,5 +173,8 @@
     </footer>
 
 </body>
+<script type="text/javascript">
+
+</script>
 
 </html>
--- list.php
+++ list.php
@@ -31,20 +31,83 @@
 </head>
 
 <body>
-    <!-- Header -->
     <header class="fixed-top">
         <nav class="navbar navbar-light header scrolling-navbar">
             <div class="container-fluid">
-                <a href="https://www.nightcatdigitalsolutions.com" class="navbar-brand">
-                    <img class="img-fluid rounded" src="./assets/titleImage.jpeg" alt="Title Image">
-                </a>
+                <div class="row" style="width:100%;">
+                    <div class="col-4">
+
+                    </div>
+                    <div class="col-4">
+                        <a href="https://www.nightcatdigitalsolutions.com" class="navbar-brand mx-auto">
+                            <img class="img-fluid rounded" src="./assets/titleImage.jpeg" alt="Title Image">
+                        </a>
+                    </div>
+                    <div class="col-4 my-auto">
+                        <div class="row">
+                            <div class="col-6 text-right">
+                                <h5></h5>
+                            </div>
+                            <div class="col-6 text-left">
+                                <button class="btn py-3 text-white loginBtn">Login/Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </nav>
     </header>
 
     <!-- Main Section -->
     <main class="main-section">
+        <div class="container">
+            <section class="container stickyContent bg-white position-fixed ">
+                <h1 class="py-2">Questions</h1>
+                <div class="row py-3 pb-4">
+                    <div class="col-6 text-left my-auto">
+                        <h4 class="searchResult">Search Result : <span>xxxxxxx</span></h4>
+                    </div>
+                    <div class="col-6 text-right my-auto">
+                        <button class="btn btn-primary py-3 px-5 text-white postQuestionBtn">Post a Question</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control px-5 py-4 rounded-lg-left searchInput" placeholder="Search....." aria-label="Search" aria-describedby="button-addon2">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary m-0 px-5 py-3 rounded-lg searchBtn" type="button" id="button-addon2">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
+            <section class="questionList">
+                <a class="card my-3" href="./forum.php">
+                    <div class="card-body p-4 px-5">
+                        <div class="row">
+                            <div class="col-3">
+                                <h2 class="card-title text-black">Question 1</h2>
+                            </div>
+                            <div class="col-7 py-auto">
+                                <h5 class="text-black">from tester 1</h5>
+                            </div>
+                            <div class="col-2 py-auto text-right">
+                                <p class="text-black">2020-09-28</p>
+                            </div>
+                        </div>
+                        <div class="row py-3">
+                            <p class="card-text text-justify" style="font-size:20px;">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt eius placeat consequuntur odio sed, aperiam deserunt commodi esse laudantium libero doloribus ullam facere,laudantium libero doloribus ullam facerelaudantium libero doloribus ullam facere ...
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </section>
+            <section id="pagination" class="py-4">
+
+            </section>
+
+        </div>
     </main>
 
     <!-- Footer -->
@@ -77,5 +140,8 @@
     </footer>
 
 </body>
+<script type="text/javascript">
+
+</script>
 
 </html>
