01d7006f8197c26c29341b39748c955e6013f788
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2021-01-07 11:51:56 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    'use strict';
+
+    var year = new Date().getFullYear();
+    $('#latestYear').text(year);
+
+
+    $.fn.inputFilter = function (inputFilter) {
+        return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    };
+})
+
+
---
+++ list.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon/favicon-16x16.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="./assets/favicon/android-chrome-192x192.png" />
+    <link rel="icon" type="image/png" sizes="512x512" href="./assets/favicon/android-chrome-512x512.png" />
+    <link rel="apple-touch-icon" href="./assets/favicon/apple-touch-icon.png">
+    <link rel="shortcut icon" href="./assets/favicon/favicon.ico">
+
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.0.0/mdb.min.css" rel="stylesheet" />
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.0.0/mdb.min.js"></script>
+
+    <link rel="stylesheet" href="./dist/style.min.css">
+    <script src="./dist/script.prod.js"></script>
+</head>
+
+<body>
+    <!-- Header -->
+    <header class="fixed-top">
+        <nav class="navbar navbar-light header scrolling-navbar">
+            <div class="container-fluid">
+                <a href="https://www.nightcatdigitalsolutions.com" class="navbar-brand">
+                    <img class="img-fluid rounded" src="./assets/titleImage.jpeg" alt="Title Image">
+                </a>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Main Section -->
+    <main class="main-section">
+
+    </main>
+
+    <!-- Footer -->
+    <footer class="page-footer font-small mdb-color blue" style="width: auto;">
+        <div class="col-md-12">
+
+            <!-- Social Link on Bottom -->
+            <div class="mb-4 pt-4 flex-center bottomLink">
+                <a class="whatsappLink">
+                    <i class="fab fa-whatsapp a-lg mr-md-5 mr-3 fa-2x"></i>
+                </a>
+                <!-- Facebook -->
+                <a class="fb-ic" href="https://www.facebook.com/nightcatdigitalsolutions">
+                    <i class="fab fa-facebook fa-lg mr-md-5 mr-3 fa-2x"> </i>
+                </a>
+                <!-- Twitter -->
+                <a class="tw-ic" href="https://twitter.com/nightcatdigital">
+                    <i class="fab fa-twitter fa-lg mr-md-5 mr-3 fa-2x"> </i>
+                </a>
+                <!--Instagram-->
+                <a class="ins-ic" href="https://www.instagram.com/nightcatdigitalsolutions/">
+                    <i class="fab fa-instagram fa-lg mr-md-5 mr-3 fa-2x"> </i>
+                </a>
+            </div>
+        </div>
+        <div class="footer-copyright text-center py-3">
+            <a href="https://www.nightcatdigitalsolutions.com">NIGHTCAT DIGITAL SOLUTIONS © 2013 -
+                <span id="latestYear"></span>. ALL RIGHTS RESERVED</a>
+        </div>
+    </footer>
+
+</body>
+
+</html>
