b5518550ef75bb046fc6aa17e0fb417e0d7c3306
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2021-01-09 17:07:35 +0800

    User table sql repair

--- sql/user.sql
+++ sql/user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS User (
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
-    passcode VARCHAR(20) NOT NULL,
+    passcode VARCHAR(500) NOT NULL,
     UNIQUE(id, username),
     PRIMARY KEY(id)
 );
