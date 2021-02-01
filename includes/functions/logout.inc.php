884ce9a63106e35291667f5273f6b981dbc0a895
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2021-02-01 15:27:57 +0800

    Initial Setup

--- includes/functions/logout.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-session_unset();
-session_destroy();
-
-header("location: ../../list.php&page=1");
-exit();
--- sql/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS User (
-    id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
-    passcode VARCHAR(500) NOT NULL,
-    UNIQUE(id, username),
-    PRIMARY KEY(id)
-);
