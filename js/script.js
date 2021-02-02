816abac75505523d3b5c0d79b092b45032592e79
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2021-02-02 10:13:05 +0800

    List Question Page done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    'use strict';
+
+    var year = new Date().getFullYear();
+    $('#latestYear').text(year);
+
+
+    $('.whatsappLink').on('click', function () {
+        var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+        if (isMobile) {
+            window.open(
+                "https://wa.me/+60126113810?text=I want to know more about Attendance System.",
+                "_blank");
+        } else {
+            window.open(
+                "https://web.whatsapp.com/send?phone=%2B60126113810&text=I want to know more about Attendance System.&app_absent=0",
+                "_blank");
+        }
+    });
+})
